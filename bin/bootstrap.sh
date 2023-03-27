71d75a206ea166525c28e858e40c48ef84ec6f31
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2023-03-27 08:24:37 -0700

    feat(ci): create action `setup-godot`; create `bootstrap.sh` script  (#6)
    * feat(ci): add a 'setup-godot' action
    
    * chore: add non-plugin addons to gitignore
    
    * chore: add .gitkeep files to addons and third_party dirs
    
    * chore: create a base 'project.godot' file
    
    * ci: define the base 'merge-pr' workflow
    
    * chore: define the base 'bootstrap.sh' script
    
    * chore: remove unnecessary gitkeep files
    
    * feat: create a 'bootstraph.sh' script for installing deps
    
    * chore: add 'godot' binary to '.gitignore'
    
    * chore: improve logging formatting for legibility
    
    * feat(tools): add helper script to install 'Godot'
    
    * feat(tools): add helper script to install 'Gut'
    
    * feat(tools): add helper script to install 'GDToolkit'
    
    * chore: update setup actions to use new helper scripts
    
    * fix: address bug moving addon folder
    
    * fix: improve handling of checking for existing download
    
    * chore: remove unnecessary param
    
    * feat: add script to detect latest gut version
    
    * feat: add script to detect target godot version
    
    * fix: refactor bootstrap script to use new helper scripts
    
    * chore: relocate 'bootstrap.sh' to 'bin' dir
    
    * chore: rename 'detect-*' scripts
    
    * chore: add godot binary to gitignore
    
    * chore: run project once and commit icon
    
    * chore: add '.gdignore' files to non-godot directories
    
    * feat: add an example test and plugin script
    
    * feat: allow gut and gdtoolkit to support 'latest' tags
    
    * feat: finish merge workflow
    
    * remove failing example tests
    
    * chore: update project config for compatibility
    
    * fix(ci): correct plugin test script
    
    * fix: ensure '.godot' directory is built
    
    * chore: add strict mode and rename godot version select script
    
    * chore: rename gut version select script
    
    * feat(tools): add a script to locate a Godot executable
    
    * feat: add the default icon to the plugin
    
    * chore: commit the project icon import file
    
    * fix: update bootstrap script to hydrate the .godot dir
    
    * fix(ci): ensure gut addon gets imported once
    
    * fix: refactor all scripts
    
    * fix(ci): use correct gdtoolkit installation
    
    * fix: correct broken step
    
    * fix(ci): add missing shell prop
    
    * fix(ci): update paths to scripts
    
    * fix(ci): wip
    
    * fix(ci): use correct version string
    
    * fix(ci): correct version check
    
    * fix(ci): update version check
    
    * fix(ci): add missing shell prop
    
    * fix(ci): increase timeout
    
    * fix(ci): pass quit command to godot

---
+++ bin/bootstrap.sh
@@ -0,0 +1,186 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)
+
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+cleanup() {
+    trap - SIGINT SIGTERM ERR EXIT
+}
+
+# ------------------------- Define: Logging functions ------------------------ #
+
+setup_colors() {
+    if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
+        NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
+    else
+        NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
+    fi
+}
+
+msg() {
+    echo >&2 -e "${1-}"
+}
+
+debug() {
+    echo >&2 -e "${BLUE}DEBUG${NOFORMAT} ${1-}"
+}
+
+err() {
+    echo >&2 -e "${RED}ERR${NOFORMAT} ${1-}"
+}
+
+info() {
+    echo >&2 -e "${GREEN}INFO${NOFORMAT} ${1-}"
+}
+
+warn() {
+    echo >&2 -e "${YELLOW}WARN${NOFORMAT} ${1-}"
+}
+
+die() {
+    local msg=$1
+    local code=${2-1} # default exit status 1
+    err "$msg"
+    exit "$code"
+}
+
+# ------------------------- Define: Utility functions ------------------------ #
+
+find_godot_executable() {
+    if [[ -x "$PWD/godot" ]]; then
+        echo "$PWD/godot"
+    elif command -v godot >/dev/null 2>&1; then
+        echo "godot"
+    fi
+}
+
+# ------------------------- Define: Script Arguments ------------------------- #
+
+usage() {
+    cat <<EOF
+Bootstraps and/or updates the repository for development.
+
+Usage: $(basename "${BASH_SOURCE[0]}") [OPTIONS]
+
+NOTE: The following dependencies are required:
+    - python3
+
+Available options:
+    -h, --help          Print this help and exit
+    -v, --verbose       Print script debug info (default=false)
+    -y, --yes           Automatically accept all prompts (default=false)
+    --[no]color         Whether to enable colored output for logging (default=true)
+EOF
+    exit
+}
+
+parse_params() {
+    ACCEPT=0
+    NO_COLOR=0
+
+    while :; do
+        case "${1-}" in
+        -h | --help) usage ;;
+        -v | --verbose) set -x ;;
+
+        --color) NO_COLOR=0 ;;
+        --no-color) NO_COLOR=1 ;;
+
+        -y | --yes) ACCEPT=1 ;;
+
+        -?*) die "Unknown option: $1" ;;
+        *) break ;;
+        esac
+        shift
+    done
+
+    args=("$@")
+
+    return 0
+}
+
+setup_colors
+parse_params "$@"
+
+if [[ ! -d "$PWD/addons" || ! -d "$PWD/bin" ]]; then
+    die "Could not find 'addons' directory; are you in the right directory?"
+fi
+
+info "Bootstrapping the repository for development..."
+
+# ------------------------------ Install: Godot ------------------------------ #
+
+echo ""
+info "Installing 'Godot' executable..."
+
+# determine the correct version to install
+GODOT_VERSION=$(./bin/utils/parse-godot-version.sh $PWD/project.godot)
+if [[ -z "$GODOT_VERSION" ]]; then
+    die "Failed to parse version from '$PWD/project.godot'!"
+fi
+
+# install the correct version
+./bin/install/godot.sh \
+    $([[ "$ACCEPT" -eq 1 ]] && echo "-y") \
+    -o $PWD/godot \
+    $GODOT_VERSION
+
+# ------------------------------- Install: Gut ------------------------------- #
+
+echo ""
+info "Installing the 'Gut' testing addon..."
+
+# determine the correct version to install
+GUT_VERSION=$(./bin/fetch/gut-version.sh)
+if [[ -z "$GUT_VERSION" ]]; then
+    die "Failed to find the latest 'Gut' version!"
+fi
+
+# if the currently installed version (if any) doesn't match, update it
+GUT_DIR="$PWD/addons/gut"
+if [[ 
+    ! -d "$GUT_DIR" ||
+    ! -f "$GUT_DIR/plugin.cfg" ||
+    "$(cat "$GUT_DIR/plugin.cfg" | grep "version=\"$GUT_VERSION\"" | sed 's/version=\"\([[:digit:]].[[:digit:]].[[:digit:]]\)\"/\1/')" != "$GUT_VERSION" ]] \
+    ; then
+    # install the correct version
+    ./bin/install/gut.sh \
+        $([[ "$ACCEPT" -eq 1 ]] && echo "-y") \
+        -o $PWD/addons \
+        $GUT_VERSION
+else
+    info "'Gut' addon already installed; skipping download!"
+fi
+
+# ---------------------------- Install: GDToolkit ---------------------------- #
+
+echo ""
+info "Installing the 'GDToolkit' formatting and linting library..."
+
+./bin/install/gdtoolkit.sh "latest"
+
+# --------------------- Build: '.godot' import directory --------------------- #
+
+if [[ ! -d ".godot" ]]; then
+    echo ""
+    info "No '.godot' import directory found; generating now..."
+
+    GODOT_BIN="$($SCRIPT_DIR/utils/find-godot.sh)"
+    if [[ -z "$GODOT_BIN" ]]; then
+        die "Failed to find a 'Godot' executable!"
+    fi
+
+    $GODOT_BIN --headless --editor --quit >/dev/null 2>&1
+
+    info "Successfully generated '.godot' import directory!"
+else
+    echo ""
+    info "Found an existing '.godot' import directory; skipping re-import process..."
+fi
+
+# ---------------------------------------------------------------------------- #
+
+echo ""
+info "Successfully bootstrapped repository!"
