680aae928c39acf6a3cbbc0c82ec33795fbc0ae7
coffeebeats <108542400+coffeebeats@users.noreply.github.com>
2026-01-28 23:18:58 -0800

    chore!: bump major version (#101)

--- example_plugin_test.gd
+++ example_plugin_test.gd
@@ -17,9 +17,5 @@ func after_all():
 	gut.p("ran run teardown", 2)
 
 
-func test_assert_eq_number_equal():
-	assert_eq("asdf", "asdf", "Should pass")
-
-
 func test_assert_true_with_true():
 	assert_true(true, "Should pass, true is true")
